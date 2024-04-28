9a26ac69231aba4f8ef47d0431fdd55f223178f6
hisamcode <hisamcode@gmail.com>
2024-04-28 15:23:19 +0700

    fix postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -4,7 +4,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \set acis_db `echo $POSTGRES_ACIS_DB`
     CREATE DATABASE :acis_db;
-    \c acis;
+    \c :acis_db;
     \set acis_user `echo $POSTGRES_ACIS_USER`
     \set acis_password `echo $POSTGRES_ACIS_PASSWORD`
     CREATE ROLE :acis_user WITH LOGIN PASSWORD :'acis_password';
