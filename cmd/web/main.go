849a99cb1b57e7a3bca780d99a15d85715a668e6
hisamcode <hisamcode@gmail.com>
2024-04-28 16:09:03 +0700

    setup connection db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,40 +1,55 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"flag"
 	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+
 type application struct {
-	slog *slog.Logger
+	config config
+	logger *slog.Logger
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./ui/htmx/pages/home.html", "./ui/htmx/bases/layout.html", "./ui/htmx/bases/head.html")
 	if err != nil {
-		app.slog.Error(err.Error())
+		app.logger.Error(err.Error())
 		return
 	}
 
 	err = t.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.slog.Error(err.Error())
+		app.logger.Error(err.Error())
 		return
 	}
 }
 func (app *application) transactionCreate(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("./ui/htmx/pages/transaction-create.html", "./ui/htmx/bases/clean.html", "./ui/htmx/bases/head.html")
 	if err != nil {
-		app.slog.Error(err.Error())
+		app.logger.Error(err.Error())
 		return
 	}
 
 	err = t.ExecuteTemplate(w, "clean-base", nil)
 	if err != nil {
-		app.slog.Error(err.Error())
+		app.logger.Error(err.Error())
 		return
 	}
 }
@@ -61,16 +76,7 @@ func (app *application) render(w http.ResponseWriter) {
 
 }
 
-func main() {
-
-	slog := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-	}))
-
-	app := application{
-		slog: slog,
-	}
-
+func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -86,15 +92,68 @@ func main() {
 	mux.HandleFunc("POST /categories", app.categoriesPost)
 	mux.HandleFunc("GET /categories/{id}/edit", app.categoriesEdit)
 	mux.HandleFunc("PUT /categories/{id}", app.categoriesPut)
+	return mux
+
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 8000, "web server port")
+	flag.StringVar(&cfg.env, "env", "development", "environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://acis:acis@localhost/acis?sslmode=disable", "postgreSQL dsn")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
 
 	server := http.Server{}
+	if app.config.env == "development" {
+		server.Addr = fmt.Sprintf("127.0.0.1:%d", app.config.port)
+	} else {
+		server.Addr = fmt.Sprintf(":%d", app.config.port)
+	}
+	server.Handler = app.routes()
+	server.IdleTimeout = time.Minute
+	server.ReadTimeout = 5 * time.Second
+	server.WriteTimeout = 10 * time.Second
+	server.ErrorLog = slog.NewLogLogger(logger.Handler(), slog.LevelError)
 
-	server.Addr = "127.0.0.1:8000"
-	server.Handler = mux
-
-	slog.Info("listen server", "addr", server.Addr)
-	err := server.ListenAndServe()
-	slog.Error(err.Error())
+	logger.Info("starting server", "addr", server.Addr, "env", app.config.env)
+	err = server.ListenAndServe()
+	logger.Error(err.Error())
 	os.Exit(1)
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 
 }
