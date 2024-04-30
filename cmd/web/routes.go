475cea2e531191f962ba863d9ae2adbccc4e7cce
hisamcode <hisamcode@gmail.com>
2024-04-30 13:31:29 +0700

    decoupling routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /register", app.register)
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /transaction/create", app.transactionCreate)
+	mux.HandleFunc("POST /transaction", app.transactionPost)
+	mux.HandleFunc("GET /categories", app.categories)
+	mux.HandleFunc("GET /categories/{id}", app.categoriesView)
+	mux.HandleFunc("GET /categories/create", app.categories)
+	mux.HandleFunc("POST /categories", app.categoriesPost)
+	mux.HandleFunc("GET /categories/{id}/edit", app.categoriesEdit)
+	mux.HandleFunc("PUT /categories/{id}", app.categoriesPut)
+	return mux
+
+}
