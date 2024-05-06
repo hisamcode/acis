2c5cfe671faf1b107c4d340dd6b423fd3cfb488b
hisamcode <hisamcode@gmail.com>
2024-05-06 19:31:09 +0700

    gsh

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,14 +1,10 @@
 
 
 htmx.onLoad(function (content) {
-  function makeSpanHtmxIndicator() {
-    var span = document.createElement("span")
-    span.classList.add("loading", "loading-spinner", "htmx-indicator")
-    return span
-  }
+  var spanHtmxIndicator = document.createElement("span")
+  spanHtmxIndicator.classList.add("loading", "loading-spinner", "htmx-indicator")
+
   var btns = content.getElementsByClassName("insert-htmx-loading")
-  var spanHtmxIndicator = makeSpanHtmxIndicator()
-  console.log(btns)
   for (let i = 0; i < btns.length; i++) {
     btns[i].appendChild(spanHtmxIndicator)
   }
