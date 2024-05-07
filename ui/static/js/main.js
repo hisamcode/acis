af0f3cae913351c2aba31c1287ae9a1194452b57
hisamcode <hisamcode@gmail.com>
2024-05-07 20:36:48 +0700

    gsh js

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,11 +1,40 @@
 
-
-htmx.onLoad(function (content) {
+function createSpanHtmxIndicator() {
   var spanHtmxIndicator = document.createElement("span")
   spanHtmxIndicator.classList.add("loading", "loading-spinner", "htmx-indicator")
+  return spanHtmxIndicator
+}
 
-  var btns = content.getElementsByClassName("insert-htmx-loading")
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].appendChild(spanHtmxIndicator)
+// function createSkeletonHtmxIndicator() {
+//   var span = document.createElement("span")
+//   span.classList.add("")
+
+//   var flex = document.createElement("div")
+//   flex.classList.add("flex", "flex-col", "gap-4", "w-full", "my-4")
+
+//   var skeleton = document.createElement("div")
+//   skeleton.classList.add("skeleton", "h-4", "w-full")
+
+//   flex.appendChild(skeleton)
+//   span.appendChild(flex)
+
+//   return span
+// }
+
+htmx.onLoad(function (content) {
+  var elementHtmxLoading = content.getElementsByClassName("insert-htmx-loading")
+  if (elementHtmxLoading.length > 0) {
+    var spanhtmxindicator = createSpanHtmxIndicator()
+    for (let i = 0; i < elementHtmxLoading.length; i++) {
+      elementHtmxLoading[i].appendChild(spanhtmxindicator)
+    }
   }
+
+  // var elementSkeleton = content.getElementsByClassName("insert-htmx-skeleton")
+  // if (elementSkeleton.length > 0) {
+  //   var elementHtmxLoading = createSkeletonHtmxIndicator()
+  //   for (let i = 0; i < elementSkeleton.length; i++) {
+  //     elementSkeleton[i].appendChild(elementHtmxLoading)
+  //   }
+  // }
 })
